bcc1609296fc11de2e744b90f179769ddede2e12
mateoo-89389438 <matacosta@fi.uba.ar>
2026-06-07 23:11:56 -0300

    Subida TP4

---
+++ soe-tp3_04-application/app/src/task_gate_d.c
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2026 Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author : Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>
+ */
+
+/********************** inclusions *******************************************/
+/* Project includes */
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Demo includes */
+#include "logger.h"
+#include "dwt.h"
+
+/* Application & Tasks includes */
+#include "board.h"
+#include "app.h"
+
+/********************** macros and definitions *******************************/
+#define G_TASK_GATE_D_CNT_INI	0ul
+
+#define TASK_GATE_D_DEL_ZERO	(pdMS_TO_TICKS(0ul))
+#define TASK_GATE_D_DEL_MAX		(pdMS_TO_TICKS(2500ul))
+
+/********************** internal data declaration ****************************/
+
+/********************** internal functions declaration ***********************/
+
+/********************** internal data definition *****************************/
+const char *p_task_gate_d_wait_2500mS		= "   ==> Task Gate A  - Wait:   2500mS";
+
+/********************** external data declaration *****************************/
+uint32_t g_task_gate_d_cnt;
+
+extern SemaphoreHandle_t xMutex_Airlock;
+extern SemaphoreHandle_t xSem_OpenRequest_D;
+extern SemaphoreHandle_t xSem_DoorClosed_D;
+/********************** external functions definition ************************/
+/* Task thread */
+void task_gate_d(void *parameters)
+{
+	/*  Declare & Initialize Task Function variables */
+	g_task_gate_d_cnt = G_TASK_GATE_D_CNT_INI;
+
+	/* Print out: Task Initialized */
+	LOGGER_INFO(" ");
+	LOGGER_INFO("  %s is running - Tick [mS] = %lu", pcTaskGetName(NULL), xTaskGetTickCount());
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for (;;)
+	{
+		if (xSemaphoreTake(xSem_OpenRequest_D, portMAX_DELAY) == pdTRUE)
+		{
+			/* Update Task Counter */
+			g_task_gate_d_cnt++;
+
+			/* Print out:  */
+			LOGGER_INFO(" ==> [Gate D] Opening request detected. Checking airlock...");
+
+			/* 2. Se intenta tomar el control de la esclusa.
+				  Si otra puerta está abierta, se bloquea acá de forma segura */
+			xSemaphoreTake(xMutex_Airlock, portMAX_DELAY);
+
+			/* 3. Esclusa libre: cambio a luz VERDE y apertura de la puerta */
+			LOGGER_INFO(" ==> [Gate D] Light: GREEN. Door OPEN. Enter the cabin.");
+
+			/* 4. Espera hasta que el simulador avise que la puerta se cerró */
+			LOGGER_INFO(" ==> [Gate D] Waiting for the door to close completely...");
+			xSemaphoreTake(xSem_DoorClosed_D, portMAX_DELAY);
+
+			/* 5. La puerta se cerró de forma segura */
+			LOGGER_INFO(" <== [Gate D] Door CLOSED and secured.");
+
+			/* 6. Se libera el Mutex para que otra puerta pueda operar */
+			xSemaphoreGive(xMutex_Airlock);
+
+			vTaskDelay(TASK_GATE_D_DEL_ZERO);
+		}
+	}
+}
+
+/********************** end of file ******************************************/
