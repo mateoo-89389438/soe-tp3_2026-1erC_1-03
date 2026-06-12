ef82dd8f514ede70a7868fcf3ce74399a3f65f8d
mateou <133512546+mateoo-89389438@users.noreply.github.com>
2026-06-12 15:22:33 -0300

    Update task_exit_b.c

--- soe-tp3_03-application/app/src/task_exit_b.c
+++ soe-tp3_03-application/app/src/task_exit_b.c
@@ -60,12 +60,18 @@ const char *p_task_exit_b_wait_2500mS		= "   ==> Task Exit B  - Wait:   2500mS";
 
 /********************** external data declaration *****************************/
 uint32_t g_task_exit_b_cnt;
-extern SemaphoreHandle_t xSem_Exit_B;
 
-extern SemaphoreHandle_t xSem_Exit_B;
-extern SemaphoreHandle_t xMutex_Crossing;
-extern SemaphoreHandle_t xSem_SpaceAvailable;
+/* Declare a variable of type QueueHandle_t. This is used to reference queues*/
+
+/* Declare a variable of type SemaphoreHandle_t (binary or counting) or mutex.
+ * This is used to reference the semaphore that is used to synchronize a thread
+ * with other thread or to ensure mutual exclusive access to...*/
+extern SemaphoreHandle_t h_sem_exit_b;
+extern SemaphoreHandle_t h_mutex_crossing;
+extern SemaphoreHandle_t h_sem_space_available;
 extern uint32_t g_crossing_cnt;
+
+/* Declare a variable of type TaskHandle_t. This is used to reference threads. */
 
 /********************** external functions definition ************************/
 /* Task thread */
@@ -81,23 +87,20 @@ void task_exit_b(void *parameters)
 	/* As per most tasks, this task is implemented in an infinite loop. */
 	for (;;)
 	{
-		/* Espera bloqueante hasta que task_test genere el evento Exit_B */
-		if (xSemaphoreTake(xSem_Exit_B, portMAX_DELAY) == pdTRUE){
+		if (xSemaphoreTake(h_sem_exit_b, portMAX_DELAY) == pdTRUE){
 			/* Update Task Counter */
 			g_task_exit_b_cnt++;
 
-			xSemaphoreTake(xMutex_Crossing, portMAX_DELAY); // Protección del recurso
-			
+			xSemaphoreTake(h_mutex_crossing, portMAX_DELAY);
 			if (g_crossing_cnt > 0)
 			{
 				g_crossing_cnt--;
 				LOGGER_INFO(" <== [Exit B] - Car exited. Cars inside: %lu", g_crossing_cnt);
-				
-				/* Se notifica que se liberó un lugar en el cruce */
-				xSemaphoreGive(xSem_SpaceAvailable);
+
+				xSemaphoreGive(h_sem_space_available);
 			}
 			
-			xSemaphoreGive(xMutex_Crossing);
+			xSemaphoreGive(h_mutex_crossing);
 			vTaskDelay(TASK_EXIT_B_DEL_ZERO);
 		}
 	}
